d5067677fb9a8a89c9dc104a517a5b75b89d320b
romain-sen <58177664+romain-sen@users.noreply.github.com>
2023-01-11 10:17:55 +0100

    feat: test deplacement joueur
    feat: deplacement joueur unfinished

--- Projet_Matteo/affichage.c
+++ Projet_Matteo/affichage.c
@@ -83,20 +83,75 @@ void affichagePlateau(t_jeu plateau)
         }
     }
 
-    for (int i = 0; i < plateau.nbr_player; i++)
+
+    gotoligcol(5, 45);
+    printf("C'est au joueur %d de joueur !", plateau.tour_de_jeu);
+    gotoligcol(7, 45);
+    printf("Vos tresors : ");
+    for (int j = 0; j < 24 / plateau.nbr_player; j++)
     {
-        gotoligcol(5, 45);
-        printf("C'est au joueur %d de joueur !", i);
-        gotoligcol(7, 45);
-        printf("Vos tresors : ");
-        for (int j = 0; j < 24 / plateau.nbr_player; j++)
+        if (j == plateau.joueurs[plateau.tour_de_jeu].index_tresors)
         {
-            if (j == plateau.joueurs[i].index_tresors)
-            {
-                Color(9 + i, 0);
+            Color(9 + plateau.tour_de_jeu, 0);
+        }
+        printf("%c ", plateau.joueurs[plateau.tour_de_jeu].main_tresors[j]);
+        Color(15, 0);
+    }
+
+}
+
+
+int testDeplacement(t_jeu plateau, int lig, int col){
+    int test = -1;
+
+    int tour = plateau.tour_de_jeu;
+    int lig_j = plateau.joueurs[tour].rang;
+    int col_j = plateau.joueurs[tour].colonne;
+    int sens = 0; //0 = haut, 1 = droite, 2 = bas, 3=gauche
+
+    // Check que le joueur ne sors par du plateau
+    if (lig < 0 || lig > || col < 0 || col > 6){
+        return -1;
+    }
+
+    if(col_j == col && lig_j < lig ){ // deplacement vers le haut
+        sens = 0;
+    } else if (col_j == col && lig_j > lig){ // deplacement vers le bas
+        sens = 2;
+    } else if(col_j < col && lig_j == lig ){ // deplacement vers la droite
+        sens = 1;
+    } else if (col_j > col && lig_j == lig){ // deplacement vers la gauche
+        sens = 3;
+    }
+
+    switch(sens){
+        case 0: {
+            //si deplacement vers le haut, on check le dessin au mileu bas de la carte du dessus
+            if(plateau.cartes[lig][col].dessin[2][1] != '#') {
+                test = 1;
             }
-            printf("%c ", plateau.joueurs[i].main_tresors[j]);
-            Color(15, 0);
+        break;
+        }
+        case 1: {
+            //si deplacement vers le haut, on check le dessin au milieu gauche de la carte a droite
+            if(plateau.cartes[lig][col].dessin[1][0] != '#') {
+                test = 1;
+            }
+        break;        }
+        case 2: {
+            //si deplacement vers le haut, on check le dessin au mileu haut de la carte en dessous
+            if(plateau.cartes[lig][col].dessin[0][1] != '#') {
+                test = 1;
+            }
+        break;
+        }
+        case 3: {
+            //si deplacement vers le haut, on check le dessin au mileu droit de la carte a gauche
+            if(plateau.cartes[lig][col].dessin[1][2] != '#') {
+                test = 1;
+            }
+        break;
         }
     }
+    return test;
 }
